9dd701b63f69b91bad508efb370b23410a979b26
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-07-20 17:54:20 +0300

    Добавил динамический url и функцию к нему в views

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('leo/', views.leo),
-    path('scorpio/', views.scorpio),
+    path('<sign_zodiac>', views.get_info_about_zodiac_sign),
 ]
