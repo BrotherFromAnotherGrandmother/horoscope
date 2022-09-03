e9889fa0690aacb366dca7ba6c74ed7d42ee7899
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-09-03 21:29:19 +0300

    Переместил роут beatiful_table из horoscope/urls в my_page/urls (из urls приложения в urls проекта)

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('type/<type_zodiac>', views.get_info_about_type_zodiac),
     path('<int:sign_zodiac>/', views.get_info_about_zodiac_sign_by_number),
     path('<str:sign_zodiac>/', views.get_info_about_zodiac_sign, name='horoscope_name'),
-    path('/beautiful_table/', views.get_info_about_beautiful_table, name='beautiful_table'),
 ]
